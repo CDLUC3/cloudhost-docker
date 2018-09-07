7bffd34a44e4404b2a6cf62b66c23a9f527e1d84
DPR2 Role Account <dpr2@uc3-mrtdat1-dev.cdlib.org>
2018-09-07 13:32:47 -0700

    There is now a default node.  No need to explicitly specify

--- test_getState.sh
+++ test_getState.sh
@@ -2,8 +2,7 @@
 
 SERVER='localhost'
 PORT='30443'
-NODE='8800'
 
-echo "curl -ks https://${SERVER}:${PORT}/cloudhost/state/${NODE}?t=xml"
-curl -ks  "https://${SERVER}:${PORT}/cloudhost/state/${NODE}?t=xml"
+echo "curl -ks https://${SERVER}:${PORT}/cloudhost/state?t=xml"
+curl -ks  "https://${SERVER}:${PORT}/cloudhost/state?t=xml"
 
